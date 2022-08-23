b69665f48b274fe57131e5bdbb5e53dae2fcf29b
Gabriel Borelli <gborelli_89@hotmail.com>
2022-08-22 23:06:32 -0300

    Improved plotbasis function

--- VectorSpace.jl
+++ VectorSpace.jl
@@ -1,15 +1,11 @@
+using StaticArrays
 using GLMakie
 GLMakie.activate!()
 
-function plotbasis(A::AbstractMatrix; sample=false, n::Int=100, mult=(1,1,1))
+function plotbasis(A::AbstractArray; sample::Bool=false, n=100, mult=(1,1,1))
    
-    if size(A,1) != 3 || size(A,2) != 3
-        error("Matrix dimension must be 3x3")
-    end
-
-    if length(mult) != 3
-        error("mult must be a tuple of length 3")
-    end
+    A = SMatrix{3,3}(A)
+    mult = SVector{3}(mult)
    
     origin = [0,0,0]
     l1 = hcat(origin,A[:,1])
@@ -21,7 +17,7 @@ function plotbasis(A::AbstractMatrix; sample=false, n::Int=100, mult=(1,1,1))
     lines!(ax, l2[1,:], l2[2,:], l2[3,:], color=:red, linewidth=3)
     lines!(ax, l3[1,:], l3[2,:], l3[3,:], color=:green, linewidth=3)
     if sample
-        x = 2.0 .* rand(3,n) .- 1.0
+        x = 2.0 .* rand(3,round(n)) .- 1.0
         x = mult .* x
         x = A*x
         scatter!(ax,x[1,:], x[2,:], x[3,:], color=:blue, markersize=20)
